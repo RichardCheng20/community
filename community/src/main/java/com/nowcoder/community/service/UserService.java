6b34f6e2a1eba1846fb71e54142afef19b738220
chengzhi <chengzhifunice@outlook.com>
2021-05-26 22:51:06 -0700

    Fourth time to commit

--- community/src/main/java/com/nowcoder/community/service/UserService.java
+++ community/src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,17 +2,124 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
 
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    //邮件包含激活码和域名项目名 把配置文件中的东西注入
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     public User findUserById(int id) {
         return userMapper.selectById(id);
     }
+    //编写注册业务, 返回内容，不能有重复账号，写个map封装多个内容
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+
+        // 空值处理
+        if (user == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+            //user里面的属性
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "账号不能为空!");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空!");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空!");
+            return map;
+        }
+
+        // 验证账号是否重复
+        User u = userMapper.selectByName(user.getUsername());
+        if (u != null) {
+            map.put("usernameMsg", "该账号已存在!");
+            return map;
+        }
+
+        // 验证邮箱
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null) {
+            map.put("emailMsg", "该邮箱已被注册!");
+            return map;
+        }
+
+        // 注册用户，即存入库中，保存之前需要对密码加密，先加上随机salt字符串然后再加密
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        //加密
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        //因为只传入了邮箱密码，其他的字段也需要从新设置
+        user.setType(0); //普通用户
+        user.setStatus(0); //没有激活
+        user.setActivationCode(CommunityUtil.generateUUID()); //激活码是随机字符串
+        //给一个系统自带的图像
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        //设置一个创建的时间
+        user.setCreateTime(new Date());
+        //添加到数据库
+        userMapper.insertUser(user);
+
+        // 激活邮件 发的是html
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        // 希望服务器用什么路径处理请求可以自己定101表示用户id，然后code是激活码 http://localhost:8080/community/activation/101/code
+        //注意注册的时候用户传过来的没有Id,
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        //模板引擎生成邮件内容
+        String content = templateEngine.process("/mail/activation", context);
+        //调用mailClient去发邮件
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+
+        //如果map为空 则说明没有问题
+        return map;
+    }
+
+    //激活需要用户Id 激活码
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1) { //表示已经激活 重复了
+            return ACTIVATION_REPEAT;
+        } else if (user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FAILURE;
+        }
+    }
+
 
 }
